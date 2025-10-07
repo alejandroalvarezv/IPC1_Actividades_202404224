cf79c134ad35b007d69969f6b879abfdf8618884
Alejandro Emmanuel Álvarez Velásquez <3476932530101@ingenieria.usac.edu.gt>
2025-10-06 20:30:47 -0600

    Nueva estructura de archivos

---
+++ actividad7/Model/Usuario.java
@@ -0,0 +1,12 @@
+package Model;
+public class Usuario {
+    private String nombre;
+
+    public Usuario(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+}
